0b6fe7195abf08a83ce2ad99ecf42f4997c8f155
glydrfreak <braedinbutler@gmail.com>
2021-05-06 11:57:47 -0600

    drop this code into the code that drives the rover

--- Magnetometer_bare_minimum.ino
+++ Magnetometer_bare_minimum.ino
@@ -2,16 +2,18 @@
 #include <Adafruit_Sensor.h>
 //#include <Wire.h>
 
-// COMPASS (MAGNETOMETER) CALIBRATION:
-float magX_min = -19.00;  // Obtain this value from calibration sketch
-float magY_min = -65.64;  // Obtain this value from calibration sketch
+// COMPASS (MAGNETOMETER) CALIBRATION: 
+//     SET THESE TO ZERO BEFORE YOU CALIBRATE AGAIN!!!
+float magX_min = -14.27;  // Obtain this value from calibration sketch
+float magY_min = -46.64;  // Obtain this value from calibration sketch
 float magZ_min = -1;      // Obtain this value from calibration sketch
-float magX_max = 68.82;   // Obtain this value from calibration sketch
-float magY_max = 52.18;   // Obtain this value from calibration sketch
+float magX_max = 45.45;   // Obtain this value from calibration sketch
+float magY_max = 11.91;   // Obtain this value from calibration sketch
 float magZ_max = 1;       // Obtain this value from calibration sketch
-bool useCalibratedMag = false;  // uses the min and max mag values, else uses magnitudes of 1.
+bool useCalibratedMag = true;  // uses the min and max mag values, else uses magnitudes of 1.
 float heading = 0;
 bool calibrating = false; // Switch this to true to print max and min mag values.
+float magneticDeclination = 30; // use this to correct for any bias errors from true north
 
 Adafruit_LSM303DLH_Mag_Unified mag = Adafruit_LSM303DLH_Mag_Unified(12345);
 
@@ -39,6 +41,10 @@ void loop() {
     if (event.magnetic.y > magY_max) magY_max = event.magnetic.y;
     if (event.magnetic.z < magZ_min) magZ_min = event.magnetic.z;
     if (event.magnetic.z > magZ_max) magZ_max = event.magnetic.z;
+    Serial.print("x: ");
+    Serial.print(event.magnetic.x);
+    Serial.print("\ty: ");
+    Serial.println(event.magnetic.y);
     // Print all minimum and maximum mag values (don't need z):
     Serial.print("minXY: ");
     Serial.print(magX_min);
@@ -52,7 +58,6 @@ void loop() {
     Serial.println(magY_max);
     Serial.println(" ");
     //Serial.println(magZ_max);
-    delay(100);
   }
   // READ THE MAGNETOMETER:
   sensors_event_t event;
@@ -61,10 +66,10 @@ void loop() {
   if(useCalibratedMag){
     float mag_x = mapf(event.magnetic.x, magX_min,magX_max, -100,100);
     float mag_y = mapf(event.magnetic.y, magY_min,magY_max, -100,100);
-    heading = (atan2(mag_y, mag_x) * 180) / PI;
+    heading = ((atan2(mag_y, mag_x) * 180) / PI) + magneticDeclination;
   }
   else{
-    heading = (atan2(event.magnetic.y, event.magnetic.x) * 180) / PI;
+    heading = ((atan2(event.magnetic.y, event.magnetic.x) * 180) / PI) + magneticDeclination;
   }
   // Normalize to 0-360
   if (heading < 0) {
